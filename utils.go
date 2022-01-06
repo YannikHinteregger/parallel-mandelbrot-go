d21eada7d54495ef5a6955c6bca1d2b0924d4eac
Yannik Hinteregger <yannik.hinteregger@qaware.de>
2022-01-06 10:10:24 +0100

    using rand lib

--- utils.go
+++ utils.go
@@ -2,20 +2,11 @@ package main
 
 import (
 	"image/color"
-	"time"
+	"math/rand"
 )
 
-// xorshift random
-
-var randState = uint64(time.Now().UnixNano())
-
-func RandUint64() uint64 {
-	randState = ((randState ^ (randState << 13)) ^ (randState >> 7)) ^ (randState << 17)
-	return randState
-}
-
 func RandFloat64() float64 {
-	return float64(RandUint64()/2) / (1 << 63)
+	return 0 + rand.Float64()*(1-0)
 }
 
 func hueToRGB(p, q, t float64) float64 {
